502bb721b74496a7c809d895e17aadcc38899203
Github0008VP <113192550+Github0008VP@users.noreply.github.com>
2026-05-06 19:53:36 +0530

    Refactor admin middleware to use Bearer token

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,21 +3,41 @@ const jwt = require('jsonwebtoken')
 
 const adminMiddleware = async (req, res,next) => {
 
-    try {
+    // With cookies
+    // try {
       
-        const token = req.cookies.token
-        if(!token){
-            return res.status(401).json({message: "Unauthorized Access!!"})
-        }
+    //     const token = req.cookies.token
+    //     if(!token){
+    //         return res.status(401).json({message: "Unauthorized Access!!"})
+    //     }
     
-        const verify = jwt.verify(token, process.env.JWT_SECRET)
-        req.admin = verify.id
-        next();
+    //     const verify = jwt.verify(token, process.env.JWT_SECRET)
+    //     req.admin = verify.id
+    //     next();
 
+    // }
+    // catch(err) {
+    //     res.status(401).json({message: "Unauthorized Access!!"})
+    // }
+
+
+    // with bearer
+
+      try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Unauthorized Access!!" });
     }
-    catch(err) {
-        res.status(401).json({message: "Unauthorized Access!!"})
-    }
+
+    const token = authHeader.split(" ")[1];
+    const verify = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.admin = verify.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized Access!!" });
+  }
 }
 
 module.exports = adminMiddleware
