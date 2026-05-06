2a99f89d2a72d49d8c6973d7de3656571e7c18bb
Github0008VP <113192550+Github0008VP@users.noreply.github.com>
2026-05-06 18:29:07 +0530

    Enforce secure cookies for token
    Updated cookie settings to always use secure flag.
    removed same site command

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -34,9 +34,7 @@ const login = async (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    maxAge: 24 * 60 * 60 * 1000
+    secure: true
 });
     
     res.status(200).json({
